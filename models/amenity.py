a0a70b15122a5accab2130f0716fd66e68902c9c
solotole <solomontoleak47@gmail.com>
2024-05-01 03:09:16 -0700

    classes update

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    name = Column(String(128), nullable=False)
