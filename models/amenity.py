6dd0ceaba234fe5ef53473ea08e28ed5ad0163d8
hadeyeenkah <tboysammy@gmail.com>
2024-05-03 12:39:16 -0700

    amenity

--- models/amenity.py
+++ models/amenity.py
@@ -2,10 +2,11 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel, Base):
     """ Amenity class model """
     __tablename__ = "amenities"
-
     name = Column(String(128), nullable=False)
+    place_amenities = relationship("PlaceAmenity", backref="amenities")
