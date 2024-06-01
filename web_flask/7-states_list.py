17b79588b532fdfca3d51591c9a26762b3feeb2f
Solotole <solomontoleak47@gmail.com>
2024-06-01 18:57:32 +0100

    serving sql data to web page

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" accessing data from sql database """
+from flask import Flask, render_template
+from models import storage
+app = Flask(__name__)
+
+
+# @app.teardown_appcontext
+# def teardown_db_session(exception):
+# """ clossing a session """
+# storage.close()
+
+
+@app.route("/states_list", strict_slashes=False)
+def acquiring_displaying():
+    """ retrieving and sorting the data """
+    states = storage.all("State")
+    sorted_list = sorted(states.values(), key=lambda state: state.name)
+    return render_template('7-states_list.html', states=sorted_list)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
