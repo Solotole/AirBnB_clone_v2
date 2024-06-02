cfb76e6a05d967fab358d6709c0dfe5d03f4e0cf
Solotole <solomontoleak47@gmail.com>
2024-06-02 02:33:30 +0100

    accessing sql data and displaying in the frontend

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -15,8 +15,7 @@ app = Flask(__name__)
 def acquiring_displaying():
     """ retrieving and sorting the data """
     states = storage.all("State")
-    sorted_list = sorted(states.values(), key=lambda state: state.name)
-    return render_template('7-states_list.html', states=sorted_list)
+    return render_template('7-states_list.html', states=states)
 
 
 if __name__ == "__main__":
