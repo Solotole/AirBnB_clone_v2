c3816806a1e9fd5ca7bb5dd33797d20cf775e746
Solotole <solomontoleak47@gmail.com>
2024-06-02 08:49:37 +0100

    everything

--- models/__init__.py
+++ models/__init__.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
 
-from models.state import State
+# from models.state import State
 from os import getenv
-from models.base_model import BaseModel
-from models.city import City
-from models.amenity import Amenity
-from models.review import Review
-from models.user import User
-from models.place import Place
+# from models.base_model import BaseModel
+# from models.city import City
+# from models.amenity import Amenity
+# from models.review import Review
+# from models.user import User
+# from models.place import Place
 
 engine_storage = getenv("HBNB_TYPE_STORAGE")
 if engine_storage == "db":
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -4,6 +4,6 @@
 -- Grant SELECT privileges on performance_schema database
 
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'Hbnb_test_pwd123!';
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
