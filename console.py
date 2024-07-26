513c59e6907712707005e7cc5e775f52f4e88e45
Solotole <solomontoleak47@gmail.com>
2024-07-26 08:28:55 +0100

    console file

--- console.py
+++ console.py
@@ -115,48 +115,19 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     def do_create(self, arg):
-        """ functona that created instances """
-        classes = {
-                'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                'State': State, 'City': City, 'Amenity': Amenity,
-                'Review': Review
-                }
+        """ functon that created instances """
         args = arg.split()
-
         if len(args) == 0:
             print("** class name missing **")
-            return
-
-        new_args = []
-        for a in args:
-            start_idx = a.find("=")
-            a = a[0: start_idx] + a[start_idx:].replace('_', ' ')
-            new_args.append(a)
-
-        if new_args[0] in classes:
-            new_instance = classes[new_args[0]]()
-            new_dict = {}
-            for a in new_args:
-                if a != new_args[0]:
-                    new_list = a.split('=')
-                    new_dict[new_list[0]] = new_list[1]
-
-            for k, v in new_dict.items():
-                if v[0] == '"':
-                    v_list = shlex.split(v)
-                    new_dict[k] = v_list[0]
-                    setattr(new_instance, k, new_dict[k])
-                else:
-                    try:
-                        if type(eval(v)).__name__ == 'int':
-                            v = eval(v)
-                    except:
-                        continue
-                    setattr(new_instance, k, v)
-            new_instance.save()
-            print(new_instance.id)
+            return False
+        if args[0] in classes:
+            new_dict = self._key_value_parser(args[1:])
+            instance = classes[args[0]](**new_dict)
         else:
             print("** class doesn't exist **")
+            return False
+        print(instance.id)
+        instance.save()
 
     def help_create(self):
         """ Help information for the create method """
